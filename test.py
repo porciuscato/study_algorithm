8720d87521833f7864f7e5697c4e9153cc04dee1
Jinhong <mpcato@naver.com>
2019-08-29 18:00:45 +0900

    dd

--- test.py
+++ test.py
@@ -1,19 +1,21 @@
 import sys
 
-sys.stdin = open('input.txt', 'r', encoding='utf-8')
+sys.stdin = open('input.txt', 'r')
 
 for T in range(1, int(input()) + 1):
     N = int(input())
-    board = [list(map(int, list(input()))) for i in range(N)]
+    base = [list(map(int, input().split())) for _ in range(N)]
+    visited = [0] * N
+    # 최소이므로? 최소여도 DFS 가능하지
 
-    arr = []
-    for i in range(int(N / 2) + 1):
-        arr.append(i)
-    for i in range(int(N / 2) - 1, -1, -1):
-        arr.append(i)
+    def dfs(start, tot):
+        for i in range(N):
+            if not visited[i]:
+                visited[i] = 1
+                hap = tot + base[start][i]
+                dfs(i, hap)
 
-    mid = int(N / 2)
-    tot = 0
-    for i in range(N):
-        tot += sum(board[i][mid - arr[i]: mid + 1 + arr[i]])
-    print('#{} {}'.format(T, tot))
+    visited[0] = 1
+    dfs(0, 0)
+
+    print('#{} {}'.format(T, 0))
