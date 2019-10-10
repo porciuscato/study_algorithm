5636838e5a64fd063cd5a370cda8b05bd961fc06
cato <mpcato@naver.com>
2019-10-10 20:00:14 +0900

    python class

--- algorithm.md
+++ algorithm.md
@@ -4580,3 +4580,74 @@ disjoint-set -> 크루스칼 알고리즘 할 때 사용(최종 부모를 설정
 
 (문제해결을 위한 창의적 알고리즘 - 고급편)
 
+
+
+
+
+
+
+
+
+클래스로 트리를 구현해보자
+
+```
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    def search(self, node, parent):
+        if node.data == parent:
+            return node
+        if not node.left and not node.right:
+            return
+        else:
+            if node.left:
+                self.search(node.left, parent)
+            if node.right:
+                self.search(node.right, parent)
+
+    def append(self, parent, data):
+        if not self.root:
+            node = Node(parent)
+            self.root = node
+        par = self.search(self.root, parent)  # root는 시작점. 여기서부터 parent를 찾자.
+        if par:
+            node = Node(data)
+            if not par.left:
+                par.left = node
+            else:
+                par.right = node
+
+    def preorder(self, node):
+        if node.data:
+            print(node.data, end=' ')
+            self.preorder(node.left)
+            self.preorder(node.right)
+
+    def inorder(self, node):
+        if node.data:
+            self.preorder(node.left)
+            print(node.data, end=' ')
+            self.preorder(node.right)
+
+    def postorder(self, node):
+        if node.data:
+            self.preorder(node.left)
+            self.preorder(node.right)
+            print(node.data, end=' ')
+
+
+data = [1, 2, 1, 3, 2, 4, 3, 5, 3, 6, 4, 7, 5, 8, 5, 9, 6, 10, 6, 11, 7, 12, 11, 13]
+tree = Tree()
+for d in range(0, len(data), 2):
+    tree.append(data[d], data[d + 1])
+tree.preorder()
+```
+
--- test.py
+++ test.py
@@ -1,27 +1,3 @@
-def preorder(node):
-    print(node, end=' ')
-    if graph[node][0]:
-        preorder(graph[node][0])
-    if graph[node][1]:
-        preorder(graph[node][1])
-
-
-def inorder(node):
-    if graph[node][0]:
-        inorder(graph[node][0])
-    print(node, end=' ')
-    if graph[node][1]:
-        inorder(graph[node][1])
-
-
-def postorder(node):
-    if graph[node][0]:
-        postorder(graph[node][0])
-    if graph[node][1]:
-        postorder(graph[node][1])
-    print(node, end=' ')
-
-
 class Node:
     def __init__(self, data):
         self.data = data
@@ -33,31 +9,50 @@ class Tree:
     def __init__(self):
         self.root = None
 
+    def search(self, node, parent):
+        if node.data == parent:
+            return node
+        if not node.left and not node.right:
+            return
+        else:
+            if node.left:
+                self.search(node.left, parent)
+            if node.right:
+                self.search(node.right, parent)
+
     def append(self, parent, data):
         if not self.root:
-            node = Node(data)
+            node = Node(parent)
             self.root = node
-        else:
-            # parent를 찾아라.
+        par = self.search(self.root, parent)  # root는 시작점. 여기서부터 parent를 찾자.
+        if par:
+            node = Node(data)
+            if not par.left:
+                par.left = node
+            else:
+                par.right = node
+
+    def preorder(self, node):
+        if node.data:
+            print(node.data, end=' ')
+            self.preorder(node.left)
+            self.preorder(node.right)
+
+    def inorder(self, node):
+        if node.data:
+            self.preorder(node.left)
+            print(node.data, end=' ')
+            self.preorder(node.right)
+
+    def postorder(self, node):
+        if node.data:
+            self.preorder(node.left)
+            self.preorder(node.right)
+            print(node.data, end=' ')
 
 
 data = [1, 2, 1, 3, 2, 4, 3, 5, 3, 6, 4, 7, 5, 8, 5, 9, 6, 10, 6, 11, 7, 12, 11, 13]
-graph = [[0] * 3 for _ in range(max(data) + 1)]
-
-# for d in range(0, len(data), 2):
-#     a = data[d]
-#     b = data[d + 1]
-#     if graph[a][0] == 0:
-#         graph[a][0] = b
-#     else:
-#         graph[a][1] = b
-#     graph[b][2] = a
-#
-# print('pre  : ', end=' ')
-# preorder(1)
-# print()
-# print('in   : ', end=' ')
-# inorder(1)
-# print()
-# print('post : ', end=' ')
-# postorder(1)
+tree = Tree()
+for d in range(0, len(data), 2):
+    tree.append(data[d], data[d + 1])
+tree.preorder()
