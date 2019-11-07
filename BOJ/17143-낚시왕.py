6558bb53d71ff851144aa134e552bcc2334edda9
cato <mpcato@naver.com>
2019-11-07 19:22:44 +0900

    fishing

--- BOJ/17143-낚시왕.py
+++ BOJ/17143-낚시왕.py
@@ -9,64 +9,70 @@ def clear():
         for col in range(1, C + 1):
             if how_many[row][col] >= 2:
                 fishes = []
+                # 같은 위치에 있는 상어들을 넣어
                 for i in range(1, M + 1):
-                    if ocean[row][col] & 1 << i:
+                    if sharks[i][0] == row and sharks[i][1] == col:
                         fishes.append(i)
+                # print(fishes)
                 mx = 0
                 for i in range(1, len(fishes)):
                     if sharks[fishes[mx]][4] < sharks[fishes[i]][4]:
                         mx = i
                 for i in range(len(fishes)):
                     if i != mx:
-                        sharks[fishes[i]][0] = 0
-                ocean[row][col] = 2 ** fishes[mx]
+                        sharks[fishes[i]][5] = 0
+                ocean[row][col] = fishes[mx]
                 how_many[row][col] = 1
 
 
 def moves():
     global sharks, ocean, how_many
+    # i는 상어의 id
     for i in range(1, M + 1):
-        if sharks[i][0]:
-            ocean[sharks[i][0]][sharks[i][1]] -= 2 ** i
-            how_many[sharks[i][0]][sharks[i][1]] -= 1
+        if sharks[i][5]:
+            # 원래 있던 자리에서 빼
+            shark_row = sharks[i][0]
+            shark_col = sharks[i][1]
+            ocean[shark_row][shark_col] -= i
+            how_many[shark_row][shark_col] -= 1
             direction = sharks[i][3]
             if direction == 1:
                 value = sharks[i][0] - sharks[i][2] % ((R - 1) * 2)
-                if 1 <= value <= R:
+                if 1 < value <= R:
                     sharks[i][0] = value
-                elif 2 - R <= value < 1:
+                elif 2 - R < value <= 1:
                     sharks[i][3] = 2
                     sharks[i][0] = 2 - value
                 else:
                     sharks[i][0] = value + 2 * (R - 1)
             elif direction == 2:
                 value = sharks[i][0] + sharks[i][2] % ((R - 1) * 2)
-                if 1 <= value <= R:
+                if 1 <= value < R:
                     sharks[i][0] = value
-                elif R < value <= 2 * R - 1:
+                elif R <= value < 2 * R - 1:
                     sharks[i][3] = 1
                     sharks[i][0] = 2 * R - value
                 else:
                     sharks[i][0] = value - 2 * (R - 1)
             elif direction == 3:
                 value = sharks[i][1] + sharks[i][2] % ((C - 1) * 2)
-                if 1 <= value <= C:
+                if 1 <= value < C:
                     sharks[i][1] = value
-                elif C < value <= 2 * C - 1:
+                elif C <= value < 2 * C - 1:
                     sharks[i][3] = 4
                     sharks[i][1] = 2 * C - value
                 else:
                     sharks[i][1] = value - 2 * (C - 1)
             elif direction == 4:
                 value = sharks[i][1] - sharks[i][2] % ((C - 1) * 2)
-                if 1 <= value <= C:
+                if 1 < value <= C:
                     sharks[i][1] = value
-                elif 2 - C <= value < 1:
+                elif 2 - C < value <= 1:
                     sharks[i][3] = 3
                     sharks[i][1] = 2 - value
                 else:
                     sharks[i][1] = value + 2 * (C - 1)
-            ocean[sharks[i][0]][sharks[i][1]] += 2 ** i
+            ocean[sharks[i][0]][sharks[i][1]] += i
             how_many[sharks[i][0]][sharks[i][1]] += 1
 
 
@@ -74,20 +80,20 @@ def fishing(king):
     global ans, sharks, ocean, how_many
     for row in range(1, R + 1):
         if ocean[row][king]:
-            pass
-            # for f in range(1, M + 1):
-            #     if ocean[row][king] & 1 << f:
-            #         ans += sharks[f][4]
-            #         sharks[f][0] = 0
-            #         ocean[row][king] = 0
-            #         how_many[row][king] = 0
-            #         return
+            # 상어 id
+            num = ocean[row][king]
+            # print(num)
+            ans += sharks[num][4]
+            ocean[row][king] = 0
+            sharks[num][5] = 0
+            how_many[row][king] = 0
+            return
 
 
 R, C, M = map(int, input().split())
 # 격자판
 ocean = [[0] * (C + 1) for _ in range(R + 1)]
-# 동일한 위치에 상어가 어디 있는지
+# 동일한 위치에 상어가 몇 마리 있는지
 how_many = [[0] * (C + 1) for _ in range(R + 1)]
 # 상어들의 정보 row, col, speed, direc, size, alive / 첫줄을 0으로
 sharks = [[0, 0, 0, 0, 0, 0]]
@@ -109,4 +115,15 @@ while king <= C:
     moves()
     clear()
     king += 1
+    # for rrow in range(1, R + 1):
+    #     for ccol in range(1, C + 1):
+    #         print(ocean[rrow][ccol], end=' ')
+    #     print()
+    # print()
+    #############
+    # for rrow in range(1, 1 + M):
+    #     for ccol in range(6):
+    #         print(sharks[rrow][ccol], end=' ')
+    #     print()
+    # print()
 print(ans)
