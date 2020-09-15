97b8ca68768f4db4fa42822d4c27d8bf4f784dc0
porciuscato <mpcato@naver.com>
2020-09-16 03:27:19 +0900

    chore: coding test 5th problem solved

--- coding_test/2021kakao/5_2.py
+++ coding_test/2021kakao/5_2.py
@@ -23,12 +23,34 @@ def sec_to_time(sec):
     return "{}:{}:{}".format(hour, minute, second)
 
 
-def solution(play_time, adv_time, logs):
-    p_time = time_to_sec(play_time)
-    a_time = time_to_sec(adv_time)
+def solve(adv_time, intervals):
+    mx = 0
+    mx_time = 0
+    length = len(intervals)
+    for start in range(length):
+        taken_time = 0
+        end = intervals[start][0] + adv_time
+        check = start
+        interval = intervals[check]
+        while interval[0] <= end:
+            if interval[1] <= end:
+                taken_time += (interval[1] - interval[0]) * interval[2]
+            else:
+                taken_time += (end - interval[0]) * interval[2]
+                break
+            check += 1
+            if check < length:
+                interval = intervals[check]
+            else:
+                break
+        if taken_time > mx:
+            mx = taken_time
+            mx_time = intervals[start][0]
+    return mx_time
 
-    # all_time = [0 for _ in range(p_time)]
-    # dp = [0 for _ in range(p_time)]
+
+def solution(play_time, adv_time, logs):
+    a_time = time_to_sec(adv_time)
 
     intervals = []
     time_records = []
@@ -53,20 +75,14 @@ def solution(play_time, adv_time, logs):
             if p_start >= record[0] and p_end < record[1]:
                 check += 1
         p_interval.append(check)
-    print(intervals)
-    print(time_records)
-    print(possible_intervals)
-    mx = 0
-    mx_time = 0
-    
-    return
+    return sec_to_time(solve(a_time, possible_intervals))
 
 
 cases = [
     ("02:03:55", "00:14:15",
      ["01:20:15-01:45:14", "00:25:50-00:48:29", "00:40:31-01:00:00", "01:37:44-02:02:30", "01:30:59-01:53:29"]),
-    # ("99:59:59", "25:00:00", ["69:59:59-89:59:59", "01:00:00-21:00:00", "79:59:59-99:59:59", "11:00:00-31:00:00"]),
-    # ("50:00:00", "50:00:00", ["15:36:51-38:21:49", "10:14:18-15:36:51", "38:21:49-42:51:45"])
+    ("99:59:59", "25:00:00", ["69:59:59-89:59:59", "01:00:00-21:00:00", "79:59:59-99:59:59", "11:00:00-31:00:00"]),
+    ("50:00:00", "50:00:00", ["15:36:51-38:21:49", "10:14:18-15:36:51", "38:21:49-42:51:45"])
 ]
 
 for case in cases:
