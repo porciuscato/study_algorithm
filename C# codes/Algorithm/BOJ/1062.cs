e9d3d485b819331dfb9e481fd94a88a44175970d
Jinhong Park <38974640+porciuscato@users.noreply.github.com>
2021-10-15 14:10:17 +0900

    boj

--- C# codes/Algorithm/BOJ/1062.cs
+++ C# codes/Algorithm/BOJ/1062.cs
@@ -1,28 +1,25 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace BOJ
 {
-    class BOJ1311
-        // 비트마스크, DP
+    class BOJ1062
     {
+        static StringBuilder sb = new StringBuilder();
         static StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
         static StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
 
         public static void start()
         {
 
-            sw.WriteLine("hello world!");
-            sw.Flush();
         }
-
     }
-
-//    class Program
-//    {
-//        static void Main()
-//        {
-//            BOJ1311.start();
-//        }
-//    }
+    //class Program
+    //{
+    //    static void Main()
+    //    {
+    //        BOJ1062.start();
+    //    }
+    //}
 }
---
+++ C# codes/Algorithm/BOJ/1094.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BOJ
+{
+    class BOJ1094
+    {
+        static int ans = 0, aim = 0;
+        const int MAX = 64;
+
+        public static void start()
+        {
+            aim = Convert.ToInt32(Console.ReadLine());
+            DivideExec(MAX);
+            Console.WriteLine(ans);
+        }
+
+        static void DivideExec(int now)
+        {
+            if (aim == 0) return;
+            
+            if (now > aim)
+            {
+                DivideExec(now / 2);
+            }
+            else
+            {
+                ans += 1;
+                aim -= now;
+                DivideExec(now / 2);
+            }
+        }
+    }
+    //class Program
+    //{
+    //    static void Main()
+    //    {
+    //        BOJ1094.start();
+    //    }
+    //}
+}
